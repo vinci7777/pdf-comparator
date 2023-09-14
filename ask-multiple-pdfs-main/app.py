e8e3f39feb8022952853e078bd00981227a8f912
Krzysztof Fitas <krzysiek.fitas91@gmail.com>
2023-09-14 18:54:39 +0200

    initial commit

---
+++ ask-multiple-pdfs-main/app.py
@@ -0,0 +1,110 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+
+     # Add print statements for debugging
+    for i, chunk in enumerate(chunks):
+        print(f"Chunk {i + 1} - Length: {len(chunk)}")
+
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    
+    # Add a print statement to check the number of text chunks
+    print(f"Number of text chunks: {len(text_chunks)}")
+    
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    
+    # Add a print statement to indicate when vectorstore creation is complete
+    print("Vectorstore creation complete")
+    
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+if 'enter_pressed' not in st.session_state:
+    st.session_state.enter_pressed = False
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Compare multiple PDFs:", page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Compare multiple PDFs: :books:")
+    user_question = st.text_area("Ask a question about your documents:")
+
+    if user_question:
+        if st.button("Submit") or st.session_state.enter_pressed:
+            if st.session_state.conversation is not None:
+                handle_userinput(user_question)
+            else:
+                st.write("Please upload PDFs and click 'Process' first.")
+            st.session_state.enter_pressed = False
+
+    if st.session_state.enter_pressed:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                vectorstore = get_vectorstore(text_chunks)
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+                
+main()
